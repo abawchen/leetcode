9feb56759de3fc11ccccf3c89c8f1474dc304b98
Yu-Lin Chen <ychen@machinezone.com>
2015-05-21 20:46:41 +0800

    Solve #128

--- 128_longest_consecutive_sequence.py
+++ 128_longest_consecutive_sequence.py
@@ -12,46 +12,38 @@ class Solution:
         if not nums:
             return 0
 
-        minNum = nums[0]
-        maxNum = nums[0]
-        dic = {nums[0]:nums[0]}
+        # http://stackoverflow.com/questions/4576115/python-list-to-dictionary
+        self.dic = dict((n, n) for n in nums)
+        self.dup = dict(self.dic)
 
-        for num in nums[1:]:
-            if num > maxNum:
-                maxNum = num
-                # maxList.append(maxNum)
-            elif num < minNum:
-                minNum = num
-
-            dic[num] = num
-
-        local = 0
         longest = 1
-        num = minNum
-        while num <= maxNum:
-            consecutive = dic.has_key(num)
-            if consecutive:
-                local += 1
-            else:
-                longest = max(longest, local)
-                consecutive = True
-                local = 0
-            num += 1
+        for key in self.dup:
+            if self.dic.has_key(key):
+                local = 1
+                local += (self.check(key+1, 1) + self.check(key-1, -1))
+            longest = max(longest, local)
 
-        return max(longest, local)
+        return longest
 
+    def check(self, key, i):
+        n = 0
+        while self.dic.has_key(key):
+            self.dic.pop(key)
+            n += 1
+            key += i
+        return n
 
 s = Solution()
 # print s.longestConsecutive([100])
+print s.longestConsecutive([0, -1]) #2
+print s.longestConsecutive([1, 2]) #2
 print s.longestConsecutive([100, 100, 101]) #2
 print s.longestConsecutive([102, 100, 100, 101]) #3
 print s.longestConsecutive([100, 4, 200, 1, 3, 2]) #4
-print s.longestConsecutive([100, 4, 1, 3, 2, 145, 146, 148, 147, 144, 300, 150]) #5
-
-# # FAILED !!
+print s.longestConsecutive([100, 4, 1, 3, 2, 145, 146, 148, 147, 144, 300, 150, 149]) #7
 print s.longestConsecutive([193, 100, 4, 200, 1, 3, 2, 195, 196, 198, 197, 194, 300, 150]) #6
 print s.longestConsecutive([193, 100, 4, 200, 1, 3, 2, 195, 196, 198, 197, 194, 300, 150, 299, 298, 297, 296, 295, 294, 293]) #8
 
 start_time = time.time()
-print s.longestConsecutive([2147483646,-2147483647,0,2,2147483644,-2147483645,2147483645])
+print s.longestConsecutive([2147483646,-2147483647,0,2,2147483644,-2147483645,2147483645]) #3
 print("--- %s seconds ---" % (time.time() - start_time))
