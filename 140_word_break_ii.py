98d452b41c8222d38899862cd4f8cae9a1cb505e
Yu-Lin Chen <ychen@machinezone.com>
2015-05-23 14:56:29 +0800

    #140 Memory Limit Exceeded

---
+++ 140_word_break_ii.py
@@ -0,0 +1,54 @@
+# Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+
+# Return all such possible sentences.
+
+# For example, given
+# s = "catsanddog",
+# dict = ["cat", "cats", "and", "sand", "dog"].
+
+# A solution is ["cats and dog", "cat sand dog"]. 
+
+
+class Solution:
+    # @param s, a string
+    # @param wordDict, a set<string>
+    # @return a string[]
+    def wordBreak(self, s, wordDict):
+        if not s or not wordDict:
+            return []
+
+        dic = {}
+        for i in xrange(len(s)):
+            for j in xrange(i+1, len(s)+1):
+                if (i == 0 or i-1 in dic) and s[i:j] in wordDict:
+                    if i == 0:
+                        dic[j-1] = [s[i:j]]
+                    elif j-1 in dic:
+                        dic[j-1].extend([v+" "+ s[i:j] for v in dic[i-1]])
+                    else:
+                        dic[j-1] = [v+" "+ s[i:j] for v in dic[i-1]]
+
+        return dic.get(len(s)-1, [])
+
+
+s = Solution()
+
+# https://leetcode.com/discuss/18248/word-break-ii-time-limit-exceeded-error-when-input-is-a
+print s.wordBreak(
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+    ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"])
+
+
+
+# print s.wordBreak("leet", ["leet"]) #['leet']
+# print s.wordBreak("leetcode", ["leet", "code"]) #['leet code']
+# print s.wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"]) #['cat sand dog', 'cats and dog']
+
+# print s.wordBreak("aaaaaaa", ["aaaa", "aa"]) #[]
+# print s.wordBreak("aaaaaaa", ["aaaa", "aaa"]) #['aaa aaaa', 'aaaa aaa']
+# print s.wordBreak("aaaaaaa", ["aaa", "aaaa", "a"]) 
+# #['aaa aaaa', 'a a a aaaa', 'aaaa aaa', 'a aaa aaa', 'aaa a aaa', 'a a a a aaa', 'a a aaaa a', 'aaa aaa a', 'a a a aaa a', 'a aaaa a a', 'a a aaa a a', 'aaaa a a a', 'a aaa a a a', 'aaa a a a a', 'a a a a a a a']
+
+# print s.wordBreak("leafjeifjifanvaeruieet", ["leet"]) #[]
+# print s.wordBreak("leafjeifjifanvaeruieet", ["leafjeifjifanvaeruieet"]) #['leafjeifjifanvaeruieet']
+# print s.wordBreak("leafjeifjifanvaeruieet", ["l", "e", "a", "f", "j", "e", "i", "f", "j", "i", "f", "a", "n", "v", "a", "e", "ruieet"]) #['l e a f j e i f j i f a n v a e ruieet']
