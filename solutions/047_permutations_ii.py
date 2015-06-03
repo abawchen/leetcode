765492c34777b8816a12a2fc9d933a23455d7337
Yu-Lin Chen <ychen@machinezone.com>
2015-06-03 11:01:03 +0800

    Refine #047, 132ms.

--- solutions/047_permutations_ii.py
+++ solutions/047_permutations_ii.py
@@ -12,17 +12,27 @@ class Solution:
         if not nums:
             return nums
 
-        nums.sort()
-        dic = {str([nums[0]]):1}
-        res = [[nums[0]]]
+        res = {(nums[0],)}
         for i in range(1, len(nums)):
-            for j in range(len(res)):
-                base = res.pop(0)
-                dic.pop(str(base))
-                for k in range(len(base)+ 1):
-                    tmp = base[:k] + [nums[i]] + base[k:]
-                    if str(tmp) not in dic:
-                        res.append(base[:k] + [nums[i]] + base[k:])
-                        dic[str(tmp)] = 1
+            tmp = set()
+            while res:
+                base = res.pop()
+                for j in range(len(base)+ 1):
+                    tmp.add(tuple(base[:j]) + (nums[i],) + tuple(base[j:]))
+            res = tmp
 
-        return res
+        return [ list(t) for t in res ]
+        
+        # dic = {str([nums[0]]):1}
+        # res = [[nums[0]]]
+        # for i in range(1, len(nums)):
+        #     for j in range(len(res)):
+        #         base = res.pop(0)
+        #         dic.pop(str(base))
+        #         for k in range(len(base)+ 1):
+        #             tmp = base[:k] + [nums[i]] + base[k:]
+        #             if str(tmp) not in dic:
+        #                 res.append(base[:k] + [nums[i]] + base[k:])
+        #                 dic[str(tmp)] = 1
+
+        # return res
