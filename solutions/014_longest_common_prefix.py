335f82060f317e1f6bd7c54f8a9928cb19f007a9
Yu-Lin Chen <ychen@machinezone.com>
2015-06-10 12:02:33 +0800

    Refine #014, 44 ms.

--- solutions/014_longest_common_prefix.py
+++ solutions/014_longest_common_prefix.py
@@ -7,18 +7,39 @@ class Solution:
         if not strs:
             return ""
 
-        lcp = strs[0]
-        for s in strs[1:]:
-            i, j, local = 0, 0, ""
-            while i < len(lcp) and j < len(s):
-                if lcp[i] != s[j]:
-                    break
-                local += lcp[j]
-                i, j = i+1, j+1
-
-            if not local:
-                return ""
-
-            lcp = local
-
+        lcp = ""
+        base = strs[0]
+        for i in range(len(base)):
+            for s in strs[1:]:
+                if i > len(s) - 1:
+                    return lcp
+                if base[i] != s[i]:
+                    return lcp
+            lcp += base[i]
         return lcp
+
+        # lcp = ""
+        # for z in zip(*strs):
+        #     bag = set(z);
+        #     if len(bag) == 1:
+        #         lcp += bag.pop()
+        #     else:
+        #         break
+        # return lcp
+
+        # 84 ms
+        # lcp = strs[0]
+        # for s in strs[1:]:
+        #     i, j, local = 0, 0, ""
+        #     while i < len(lcp) and j < len(s):
+        #         if lcp[i] != s[j]:
+        #             break
+        #         local += lcp[j]
+        #         i, j = i+1, j+1
+
+        #     if not local:
+        #         return ""
+
+        #     lcp = local
+
+        # return lcp
