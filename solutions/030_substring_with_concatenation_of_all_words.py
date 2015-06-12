a20371f48a98cfe35713eb66e54f2b2430757d50
Yu-Lin Chen <ychen@machinezone.com>
2015-06-12 17:09:55 +0800

    Memory Limit Exceeded #030

--- solutions/030_substring_with_concatenation_of_all_words.py
+++ solutions/030_substring_with_concatenation_of_all_words.py
@@ -14,17 +14,61 @@ class Solution:
     # @param {string[]} words
     # @return {integer[]}
     def findSubstring(self, s, words):
+        # from collections import defaultdict
+        # dic = defaultdict(list)
+
+        # indices, length, totalLength = [], len(words[0]), len(words[0])*len(words)
+        # for i in range(len(s)):
+        #     j, k, dupWords = i, i, words[:]
+
+        #     if totalLength > len(s[j:]):
+        #         # print "dic:", dic
+        #         return indices
+
+        #     if j in dic:
+        #         for w in dic[j]:
+        #             print "w:", w
+        #             # j += len(w)
+        #             dupWords.remove(w)
+        #         print "(j, dic[j]):", (j, dic[j])
+
+        #     while s[j:j+length] in dupWords:
+        #         # print "(i, j):", (i, j)
+        #         if dic[j+length]:
+        #             dic[j+length] += dic[j]
+        #         else:
+        #             dic[j+length] += [s[j:j+length]]
+        #         dupWords.remove(s[j:j+length])
+        #         j += length
+
+        #     print "(i, dupWords):", (i, dupWords)
+        #     if not dupWords:
+        #         # print "new k:", k
+        #         indices.append(k)
+
+
+        # brute force
         # print (len(s), len(words))
-        
-        indices = []
-        length = len(words[0])
+        # print words
+        indices, failed = [], set()
+        length, totalLength = len(words[0]), len(words[0])*len(words)
         for i in range(len(s)):
-            j, k, used = i, i, set()
-            while s[j:j+length] in words and s[j:j+length+1] not in used:
-                used.add(s[j:j+length])
+            j, dupWords = i, words[:]
+
+            if totalLength > len(s[j:]):
+                return indices
+
+            if s[i:i+totalLength] in failed:
+                continue
+
+            while s[j:j+length] in dupWords:
+                dupWords.remove(s[j:j+length])
                 j += length
-            if len(used) == len(words):
-                indices.append(k)
+
+            if not dupWords:
+                indices.append(i)
+            else:
+                failed.add(s[i:i+totalLength])
 
         return indices
 
