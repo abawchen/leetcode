a2ef81a39d8d0b807be9551fa17703ceb8c8b6f8
Yu-Lin Chen <ychen@machinezone.com>
2015-07-02 10:00:42 +0800

    Modify test case of #045

--- tests/045.py
+++ tests/045.py
@@ -10,17 +10,17 @@ class Test(unittest.TestCase):
     def test_jump(self):
         s = solutions.Solution()
 
-        # nums = [2, 3, 1, 1, 4]
-        # self.assertEquals(s.jump(nums), 2)
+        nums = [2, 3, 1, 1, 4]
+        self.assertEquals(s.jump(nums), 2)
 
-        # nums = [20]
-        # self.assertEquals(s.jump(nums), 0)
+        nums = [20]
+        self.assertEquals(s.jump(nums), 0)
 
-        # nums = [2,0]
-        # self.assertEquals(s.jump(nums), 1)
+        nums = [2, 0]
+        self.assertEquals(s.jump(nums), 1)
 
-        # nums = [2, 5, 0, 0]
-        # self.assertEquals(s.jump(nums), 2)
+        nums = [2, 5, 0, 0]
+        self.assertEquals(s.jump(nums), 2)
 
         nums = [2, 4, 1, 6, 4, 1, 1, 1, 1, 1]
         self.assertEquals(s.jump(nums), 3)
