be0faf4fd9a933bdeec93a9ec27c783eccec1f53
abawchen <abaw.chen@gmail.com>
2018-06-06 15:25:20 +0800

    Failed at nthUglyNumber(7)

--- solutions/264_ugly_number_ii.py
+++ solutions/264_ugly_number_ii.py
@@ -1,12 +1,59 @@
 class Solution:
 
+    ugly_nums = [1]
+    ugly_status = [(1, 0)]
+    ugly_bases = [2, 3, 5]
+
+    def nthUglyNumber(self, n):
+        import sys
+        while n > len(Solution.ugly_nums):
+            mi = -1
+            bi = 3
+            minimum = sys.maxint
+            print('*' * 10)
+            for i, (u, b) in enumerate(Solution.ugly_status):
+                print(minimum, i, (u, b))
+                if b > bi:
+                    break
+
+                m = u * Solution.ugly_bases[b]
+                if m < minimum:
+                    print('here:', m)
+                    minimum = m
+                    mi = i
+                    bi = b
+                elif m == minimum:
+                    if b == 2:
+                        Solution.ugly_status.pop(i)
+                    else:
+                        Solution.ugly_status[i] = (m, b + 1)
+                    break
+                elif m > minimum:
+                    break
+
+            (u, b) = Solution.ugly_status[mi]
+            if b == 2:
+                Solution.ugly_status.pop(mi)
+            else:
+                Solution.ugly_status[mi] = (u, b + 1)
+            # print((m, 0))
+            print(Solution.ugly_status)
+            print(m)
+            print('&' * 10)
+            Solution.ugly_nums += [minimum]
+            Solution.ugly_status += [(minimum, 0)]
+
+        print(Solution.ugly_nums)
+        return Solution.ugly_nums[n-1]
+
+    '''
     ugly_nums = [1, 2, 3, 4, 5]
     def nthUglyNumber(self, n):
         if n < 5:
             return Solution.ugly_nums[n-1]
-        
+
         while n > len(Solution.ugly_nums):
-            c = Solution.ugly_nums[-1]            
+            c = Solution.ugly_nums[-1]
             found = False
             while not found:
                 c += 1
@@ -17,3 +64,4 @@ class Solution:
                         break
 
         return Solution.ugly_nums[n-1]
+    '''
--- tests/264.py
+++ tests/264.py
@@ -4,8 +4,29 @@ from math import pow
 
 def test_solution():
     s = solution.Solution()
+    '''
     assert s.nthUglyNumber(1) == 1
+    assert s.nthUglyNumber(2) == 2
+    assert s.nthUglyNumber(3) == 3
+    assert s.nthUglyNumber(4) == 4
+    assert s.nthUglyNumber(5) == 5
     assert s.nthUglyNumber(6) == 6
-    assert s.nthUglyNumber(10) == 12
-    assert s.nthUglyNumber(20) == 36
-    assert s.nthUglyNumber(1690) == 1
+    '''
+    assert s.nthUglyNumber(7) == 8
+    # assert s.nthUglyNumber(10) == 12
+    # assert s.nthUglyNumber(20) == 36
+    # assert s.nthUglyNumber(1690) == 1
+
+'''
+nums
+[1]
+[1, 2]
+[1, 2, 3]
+'''
+
+'''
+status
+[(1, 0)]
+[(1, 1), (2, 0)]
+[(1, 2), (2, 0), (3, 0)]
+'''
