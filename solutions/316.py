501b9540e46adee8069212b92aaf06e3ac1426d0
AbawChen <abaw.chen@gmail.com>
2018-06-22 10:16:58 +0800

    WA

--- solutions/316.py
+++ solutions/316.py
@@ -1,4 +1,8 @@
 class Solution:
+
+    def larger(self, s, t):
+        return list(filter(lambda x: x > t, s))
+
     def removeDuplicateLetters(self, s):
         """
         :type s: str
@@ -8,6 +12,47 @@ class Solution:
             return ''
 
         from collections import defaultdict
+        seen = defaultdict(set)
+        position = defaultdict(list)
+        for i, v in enumerate(s[::-1]):
+            p = len(s)-i-1
+            position[v].insert(0, p)
+            if p != len(s)-1:
+                previous = seen[p+1].copy()
+                seen[p] = previous
+                seen[p].add(s[p+1])
+
+        letters = sorted(position.keys())
+        tmp = [-1, position[letters[0]][0], len(s)]
+        for letter in letters[1:]:
+            print(tmp)
+            ps = position[letter]
+            m = 0
+            n = 0
+            p = -1
+            while p == -1:
+
+                if n == len(tmp)-2:
+                    p = n+1
+                elif ps[m] > tmp[n] and ps[m] < tmp[n+1]:
+                    m_seen = self.larger(seen[ps[m]], letter)
+                    n_seen = self.larger(seen[tmp[n+1]], letter)
+                    if len(m_seen) > len(n_seen):
+                        p = n+1
+                    elif m == len(ps)-1:
+                        p = n+1
+                    else:
+                        m += 1
+                else:
+                    n += 1
+
+            tmp.insert(p, ps[m])
+        print(tmp)
+        return ''.join(list(map(lambda i: s[i], tmp[1:-1])))
+
+        # print(seen)
+        # print(position)
+        """
         d = defaultdict(list)
         for i, v in enumerate(s):
             d[v].append(i)
@@ -33,14 +78,5 @@ class Solution:
                 print(e, i)
                 ans.insert(e, ps[i-1])
         print(ans)
-        return ''.join(list(map(lambda i: s[i], ans)))
-        """
-        idx = set()
-        for v in l:
-            idx.add(d[v][-1])
-        ans = ''
-        for i, v in enumerate(s):
-            if i in idx:
-                ans += v
-        return ans
+        return ''.join(list(map(lambda i: s[i], ans))
         """
--- tests/316.py
+++ tests/316.py
@@ -3,12 +3,18 @@ solution = __import__('solutions.316', fromlist='*')
 
 def test_solution():
     s = solution.Solution()
+    """
     assert s.removeDuplicateLetters('') == ''
+    assert s.removeDuplicateLetters('ab') == 'ab'
     assert s.removeDuplicateLetters('a') == 'a'
     assert s.removeDuplicateLetters('bcabc') == 'abc'
     assert s.removeDuplicateLetters('bca') == 'bca'
     assert s.removeDuplicateLetters('bcab') == 'bca'
+    assert s.removeDuplicateLetters('bcacb') == 'acb'
     assert s.removeDuplicateLetters('cbacdcbc') == 'acdb'
+    """
+    # assert s.removeDuplicateLetters('ccacbaba') == 'acb'
+    assert s.removeDuplicateLetters('yioccqiorhtoslwlvfgzycahonecugtatbyphpuunwvaalcpndabyldkdtzfjlgwqk') == 'ciorhsaebpunvdyktzfjlgwq'
     '''
     cbacdcbc
       ^^^ ^
