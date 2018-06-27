42635fac6b180ae1196647911edf11179bfca9c3
abawchen <abaw.chen@gmail.com>
2018-06-27 15:24:41 +0800

    WA: 210, not consider loop

---
+++ tests/210.py
@@ -0,0 +1,15 @@
+solution = __import__('solutions.210', fromlist='*')
+
+
+def test_solution():
+    s = solution.Solution()
+    assert s.findOrder(4, [[1,0],[2,0],[3,1],[3,2]]) == [0,2,1,3]
+    assert s.findOrder(2, [[1,0]]) == [0,1]
+    assert s.findOrder(0, []) == []
+    assert s.findOrder(1, []) == [0]
+    assert s.findOrder(2, []) == [0,1]
+    assert s.findOrder(3, [[1,0]]) == [0,1,2]
+    assert s.findOrder(5, [[1,0],[2,1],[3,1],[4,2]]) == [0,1,2,4,3]
+    assert s.findOrder(5, [[1,0],[2,0],[3,1],[4,3],[4,2]]) == [0,1,2,3,4]
+    assert s.findOrder(2, [[0,1],[1,0]]) == []
+    assert s.findOrder(3, [[0,2],[1,2],[2,0]]) == []
