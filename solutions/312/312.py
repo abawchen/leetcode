c91452de3b932b650542c536c43613c0f175ad1a
Abaw Chen <abaw.chen@ubiik.com>
2022-01-26 22:01:41 +0800

    minor: move to 312

--- solutions/312/312.py
+++ solutions/312/312.py
@@ -18,6 +18,7 @@ Explanation: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
              coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
 """
 
+import unittest
 
 class Ballon:
 
@@ -138,3 +139,50 @@ class Solution:
                 total -= 1
         return coins
     '''
+
+class SolutionTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_01(self):
+        assert self.solution.maxCoins([10]) == 10
+        assert self.solution.maxCoins([3,8]) == 3*8 + 8
+        assert self.solution.maxCoins([3,10,3]) == (90 + 9 + 3)
+        """
+        """
+        assert self.solution.maxCoins([3,5,8]) == 152 #(3*5*8 + 3*8 + 8)
+        assert self.solution.maxCoins([1,5,8]) == (5 + 40 + 8)
+        assert self.solution.maxCoins([8,5,3]) == 152
+        assert self.solution.maxCoins([3,1,5,8]) == (15 + 152)
+        assert self.solution.maxCoins([0,3,1,5,8]) == 167
+        assert self.solution.maxCoins([3,0,1,5,8]) == 167
+
+    def test_02(self):
+        pass
+        # assert s.maxCoins([3,2,5,8]) == (3*2*5 + 152)
+        # assert s.maxCoins([3,5,8]) == 120 + 24 + 8 # 30
+        # assert s.maxCoins([3,2,8]) == 48 + 24 + 8  # 80
+        # # assert s.maxCoins([3,1,2,5,8]) == (6 + 30 + 152)
+        """
+        assert s.maxCoins([1,3,5,8]) == (3 + 152)
+        assert s.maxCoins([1,3,2,5,8]) == (3 + 30 + 152)
+        assert s.maxCoins([9,76,64,21]) == 116718 # (102144 + 14574)
+        assert s.maxCoins([9,76,21]) == 14574
+        assert s.maxCoins([76,64,21,21]) == 137332 # (64*76*21 + 35188)
+        # """
+        """
+        ==> 先砍 64
+            76*64*21 + 76*21*21 + 76*21 + 76
+                       ^^^^^^^^
+        ==> 先砍 21
+            64*21*21 + 76*64*61 + 76*21 + 76
+            ^^^^^^^^
+        """
+        """
+        assert s.maxCoins([76,21,21]) == 35188
+        assert s.maxCoins([76,64,21]) == 103816
+        """
+
+if __name__ == '__main__':
+    unittest.main()
