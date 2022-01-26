0930d5a8a5ce0d08d9811ffc42fdbad07c1e38a4
Abaw Chen <abaw.chen@ubiik.com>
2022-01-26 13:39:22 +0800

    Time Limit Exceeded

---
+++ solutions/502/502.py
@@ -0,0 +1,70 @@
+import unittest
+
+
+class Solution:
+
+    def findMaximizedCapital(self, k: int, w: int, profits: list[int], capital: list[int]) -> int:
+        projects = sorted(zip(capital, profits), key = lambda p : p[0])
+        for r in range(k):
+            profit = 0
+            idx = -1
+            for i, p in enumerate(projects):
+                if w >= p[0]:
+                    if p[1] > profit:
+                        idx = i
+                        profit = p[1]
+                else:
+                    break
+            if idx == -1:
+                break
+            else:
+                projects.pop(idx)
+                w += profit
+        return w
+
+
+
+class SolutionTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_01(self):
+        k = 2
+        w = 0
+        profits = [1,2,3]
+        capital = [0,1,1]
+        expected = 4
+        got = self.solution.findMaximizedCapital(k, w, profits, capital)
+        self.assertEqual(expected, got)
+
+    def test_02(self):
+        k = 3
+        w = 0
+        profits = [1,2,3]
+        capital = [0,1,2]
+        expected = 6
+        got = self.solution.findMaximizedCapital(k, w, profits, capital)
+        self.assertEqual(expected, got)
+
+    def test_03(self):
+        k = 3
+        w = 10
+        profits = [1,2,3]
+        capital = [0,1,2]
+        expected = 16
+        got = self.solution.findMaximizedCapital(k, w, profits, capital)
+        self.assertEqual(expected, got)
+
+    def test_04(self):
+        k = 3
+        w = 0
+        profits = [1,2,3]
+        capital = [4,1,2]
+        expected = 0
+        got = self.solution.findMaximizedCapital(k, w, profits, capital)
+        self.assertEqual(expected, got)
+
+
+if __name__ == '__main__':
+    unittest.main()
